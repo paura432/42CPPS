36fc2cb71350265f723594010e72a395287ff21f
Pau Ramos <pauramosimo@gmail.com>
2025-04-27 19:54:25 +0200

    done ex00 and ex 01

---
+++ cpp02/ex01/includes/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pau <pau@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 19:26:51 by pau               #+#    #+#             */
+/*   Updated: 2025/04/27 14:41:59 by pau              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#   include <iostream>
+
+class Fixed {
+    private:
+    
+    int _value;
+    static const int _fracint = 8;
+
+    public:
+    
+    Fixed();
+    Fixed(const int value);
+    Fixed(const float num);
+    Fixed(const Fixed& other);
+    Fixed& operator=(const Fixed& rhs); 
+    ~Fixed();
+    
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+};
+
+#endif
---
+++ cpp02/ex01/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pau <pau@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 13:28:27 by pau               #+#    #+#             */
+/*   Updated: 2025/04/27 13:30:00 by pau              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+int main(void) {
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+
+    return 0;
+}
